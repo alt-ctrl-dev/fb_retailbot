d52cc547aaf0bab1b74aef36500399768f58a8c6
Coutinho, Reuben <reuben.coutinho@accenture.com>
2017-07-24 10:14:56 +1000

    Refactor: updated log

--- app.js
+++ app.js
@@ -887,7 +887,7 @@ function callSendAPI(messageData) {
 					recipientId);
 			}
 		} else {
-			console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+			console.error("Failed calling Send API", error.message);
 		}
 	});
 }
